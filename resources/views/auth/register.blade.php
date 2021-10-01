17e8ba1f1b87c2e7663d7602a648cd5ff113de17
Moholand <moho.akbarzadeh@gmail.com>
2021-10-01 14:30:19 +0330

    convert register page to persian

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -6,8 +6,8 @@
 
       <div class="wrap-breadcrumb">
         <ul>
-          <li class="item-link"><a href="{{ route('home') }}" class="link">home</a></li>
-          <li class="item-link"><span>Register</span></li>
+          <li class="item-link"><span>ثبت نام</span></li>
+          <li class="item-link"><a href="{{ route('home') }}" class="link">خانه</a></li>
         </ul>
       </div>
       <div class="row">
@@ -19,30 +19,31 @@
                 <form class="form-stl" action="{{ route('register') }}" name="frm-login" method="POST" >
                   @csrf
                   <fieldset class="wrap-title">
-                    <h3 class="form-title">Create an account</h3>
-                    <h4 class="form-subtitle">Personal infomation</h4>
+                    <h3 class="form-title">ایجاد حساب کاربری</h3>
+                    <h4 class="form-subtitle">اطلاعات کاربر</h4>
                   </fieldset>									
                   <fieldset class="wrap-input">
-                    <label for="frm-reg-lname">Name*</label>
-                    <input type="text" id="frm-reg-lname" name="name" placeholder="Last name*" :value="name" required autofocus autocomplete="name">
+                    <label for="frm-reg-lname">* نام</label>
+                    <input type="text" id="frm-reg-lname" name="name" placeholder="نام خود را وارد نمایید" :value="name" required autofocus autocomplete="name">
                   </fieldset>
                   <fieldset class="wrap-input">
-                    <label for="frm-reg-email">Email Address*</label>
-                    <input type="email" id="frm-reg-email" name="email" placeholder="Email address" :value="email">
+                    <label for="frm-reg-email">* ایمیل</label>
+                    <input type="email" id="frm-reg-email" name="email" placeholder="ایمیل خود را وارد نمایید" :value="email">
                   </fieldset>
                   
                   <fieldset class="wrap-title">
-                    <h3 class="form-title">Login Information</h3>
+                    <h3 class="form-title">اطلاعات ورود</h3>
                   </fieldset>
-                  <fieldset class="wrap-input item-width-in-half left-item ">
-                    <label for="frm-reg-pass">Password *</label>
-                    <input type="password" id="frm-reg-pass" name="password" placeholder="Password" required autocomplete="new-password">
+                  <fieldset class="wrap-input item-width-in-half left-item">
+                    <label for="frm-reg-cfpass">* تایید رمز عبور</label>
+                    <input type="password" id="frm-reg-cfpass" name="password_confirmation" placeholder="تایید رمز عبور" required autocomplete="new-password">
                   </fieldset>
-                  <fieldset class="wrap-input item-width-in-half ">
-                    <label for="frm-reg-cfpass">Confirm Password *</label>
-                    <input type="password" id="frm-reg-cfpass" name="password_confirmation" placeholder="Confirm Password" required autocomplete="new-password">
+                  <fieldset class="wrap-input item-width-in-half">
+                    <label for="frm-reg-pass">* رمز عبور</label>
+                    <input type="password" id="frm-reg-pass" name="password" placeholder="رمز عبور" required autocomplete="new-password">
                   </fieldset>
-                  <input type="submit" class="btn btn-sign" value="Register" name="register">
+                  
+                  <input type="submit" class="btn btn-sign" value="ثبت نام" name="register">
                 </form>
               </div>											
             </div>
