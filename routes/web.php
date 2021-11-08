b44c79b16cb3539cce723be73595002c8e9d1f9f
Moholand <moho.akbarzadeh@gmail.com>
2021-11-08 17:10:52 +0330

    move product from wishlist tto cart

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,7 @@ use App\Http\Livewire\SearchComponent;
 use App\Http\Livewire\DetailsComponent;
 use App\Http\Livewire\CategoryComponent;
 use App\Http\Livewire\CheckoutComponent;
+use App\Http\Livewire\WishlistComponent;
 use App\Http\Livewire\Admin\AdminSaleComponent;
 use App\Http\Livewire\Admin\AdminProductComponent;
 use App\Http\Livewire\User\UserDashboardComponent;
@@ -51,6 +52,8 @@ Route::get('/product/{slug}', DetailsComponent::class)->name('detail');
 Route::get('/product-category/{category_slug}', CategoryComponent::class)->name('product.category');
 
 Route::get('/search', SearchComponent::class)->name('search');
+
+Route::get('/wishlist', WishlistComponent::class)->name('wishlist');
 
 // Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
 //     return view('dashboard');
